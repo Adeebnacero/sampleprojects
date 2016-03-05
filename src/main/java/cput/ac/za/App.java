fe6230f92efcfaa6d0f7c35cac30d7f03de72c7e
Adeeb <adeebnacero@gmail.com>
2016-03-05 16:28:07 +0200

    app.java file updadted

--- src/main/java/cput/ac/za/App.java
+++ src/main/java/cput/ac/za/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "TP2 2016" );
     }
 }
+git
---
+++ src/main/java/cput/ac/za/sample3.java
@@ -0,0 +1,7 @@
+package cput.ac.za;
+
+/**
+ * Created by Adeebo on 2016/03/05.
+ */
+public class sample3 {
+}
